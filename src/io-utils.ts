507b77c60dea4a6469b2da76f9449b5c92c662f7
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2020-04-01 12:14:11 -0600

    trying these logs...

--- src/io-utils.ts
+++ src/io-utils.ts
@@ -2,16 +2,9 @@ import {Dirent} from "fs";
 import fs from "fs";
 
 export function findReleaseFile(releaseDir: string): Dirent | undefined {
-    console.log("CAN WE SEE THIS:", fs.readdirSync(releaseDir, {withFileTypes: true}))
     const releaseFiles = fs.readdirSync(releaseDir, {withFileTypes: true})
-        .filter(item => {
-            console.log("First filter:", item)
-            return !item.isDirectory()
-        })
-        .filter(item => {
-            console.log("Second filter:", item)
-            return item.name.endsWith(".apk") || item.name.endsWith(".aab")
-        });
+        .filter(item => !item.isDirectory())
+        .filter(item => item.name.endsWith(".apk") || item.name.endsWith(".aab"));
 
     if (releaseFiles.length > 0) {
         return releaseFiles[0]
