e3683b07be45d288e75e0864ea6687d487ecd5fb
Kyle Czajkowski <38336076+Kyle-Ski@users.noreply.github.com>
2020-04-01 11:33:42 -0600

    Update io-utils.ts

--- src/io-utils.ts
+++ src/io-utils.ts
@@ -3,8 +3,14 @@ import fs from "fs";
 
 export function findReleaseFile(releaseDir: string): Dirent | undefined {
     const releaseFiles = fs.readdirSync(releaseDir, {withFileTypes: true})
-        .filter(item => !item.isDirectory())
-        .filter(item => item.name.endsWith(".apk") || item.name.endsWith(".aab"));
+        .filter(item => {
+            console.log("First filter:", item)
+            return !item.isDirectory()
+        })
+        .filter(item => {
+            console.log("Second filter:", item)
+            return item.name.endsWith(".apk") || item.name.endsWith(".aab")
+        });
 
     if (releaseFiles.length > 0) {
         return releaseFiles[0]
