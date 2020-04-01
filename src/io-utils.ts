c1a5a2971b230a695e8a8a9c44fe9fdd9ecf817b
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2020-04-01 11:40:42 -0600

    more logs..

--- src/io-utils.ts
+++ src/io-utils.ts
@@ -2,6 +2,7 @@ import {Dirent} from "fs";
 import fs from "fs";
 
 export function findReleaseFile(releaseDir: string): Dirent | undefined {
+    console.log("CAN WE SEE THIS:", fs.readdirSync(releaseDir, {withFileTypes: true}))
     const releaseFiles = fs.readdirSync(releaseDir, {withFileTypes: true})
         .filter(item => {
             console.log("First filter:", item)
